5da0dce3f85a0dd15e0fb5a48490157d892a16f8
pringleActivity <pringle_activity@outlook.com>
2018-07-16 21:59:47 +0300

    first commit

---
+++ app/src/main/java/com/example/vadym/alertdialogdemo/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.vadym.alertdialogdemo;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnShowDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnShowDialog = (Button) findViewById(R.id.btn_show_dialog);
+        btnShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog();
+            }
+        });
+
+    }
+
+    private void showDialog() {
+        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+        View v = inflater.inflate(R.layout.dialog, null);
+
+        AlertDialog.Builder builderDialog = new AlertDialog.Builder(MainActivity.this);
+        builderDialog.setView(v)
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alertDialog = builderDialog.create();
+        alertDialog.show();
+    }
+}
